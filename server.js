17d256c45a4e141ac06788602588c6297b731055
5l3y3r <64905248+mrrobot335@users.noreply.github.com>
2025-06-11 08:46:03 -0700

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
 
 const server = http.createServer(app);
 const io = new Server(server, {
